76311590285a48565e80456087d3802986f7d94f
Jessi <jessi.sandoval93@gmail.com>
2018-09-29 14:58:23 -0500

    Add  CRUD proyect

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+  
+
+
+  def create
+    @user=User.new(users_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user=User.find(params[:id]) 
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(users_params)
+      redirect_to users_path
+    else
+      render:edit 
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :surname, :document, :phone )
+  end
+  
+end
